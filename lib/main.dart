00782ae1404f2b0fb4ef3a3c99c834e14baf1c6b
Ignacy Trocki <ignacy.trocki@wp.pl>
2023-02-08 12:50:51 +0100

    added working focus button to map

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
   @override 
   Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Temple Finder',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
       home: MapScreen(),
     );
@@ -29,26 +29,43 @@ class _MapScreenState extends State<MapScreen> {
     zoom: 14,
   );
 
+  late GoogleMapController _googleMapController;
+
+  @override 
+  void dispose(){
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext content) {
     return Scaffold(
-      backgroundColor: Color(0xFF0f172a),
-        body: Padding(padding:EdgeInsets.all(10), child: Column(
+      backgroundColor: const Color(0xFF0f172a),
+        body: Padding(padding:const EdgeInsets.all(10), child: Column(
           children: [
             SizedBox(
               width: MediaQuery.of(context).size.width,  // or use fixed size like 200
               height: MediaQuery.of(context).size.height-100,
-              child: const GoogleMap(
+              child: Scaffold(
+                body:  GoogleMap(
                       myLocationButtonEnabled: false,
                       zoomControlsEnabled: false,
                       initialCameraPosition: _initialCameraPosition,
-                    )
+                      onMapCreated: (controller) => _googleMapController = controller,
+                      ), 
+                      floatingActionButton: FloatingActionButton(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        foregroundColor: Colors.black,
+                        onPressed: ()=> _googleMapController.animateCamera(CameraUpdate.newCameraPosition(_initialCameraPosition)),
+                        child: const Icon(Icons.center_focus_strong),
+                      ),
+              )    
             ),
-            SizedBox(height:10),
+            const SizedBox(height:10),
             SizedBox(
               width: 180,
               height: 60,
-              child:ElevatedButton(onPressed: ()=>{}, style: ElevatedButton.styleFrom(primary: Color(0xFF374151)),child: Text('Wyszukaj Nabożeństwa',textAlign: TextAlign.center, style: TextStyle(fontSize: 20)))
+              child:ElevatedButton(onPressed: ()=>{}, style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF374151)),child: const Text('Wyszukaj Nabożeństwa',textAlign: TextAlign.center, style: TextStyle(fontSize: 20)))
             )
     ])
     ));
