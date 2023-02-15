f7cd728a1257c30010cf92bd5731d7c2f6237b89
Sebastian <sebastianzubrycki@gmail.com>
2023-02-15 15:50:07 +0100

    Data file added

---
+++ lib/data.dart
@@ -0,0 +1,704 @@
+List<List<Object>> data = [
+  [
+    'Parafia pw. św. Teresy od Dzieciątka Jezus',
+    'ul. Park Dyrekcyjny 2 Białowieża 17-230',
+    ['09:00', '11:00', '18:00'],
+    ['18:00']
+  ],
+  [
+    'Parafia pw. bł. Bolesławy Lament',
+    'ul. M. Dąbrowskiej 3 15-702 Białystok',
+    ['07:30', '10:00', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Chrystusa Króla',
+    'Pl. Ks. Adolfa Ołdziejewskiego 1 15-545 Białystok',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['18:00']
+  ],
+  [
+    'Parafia pw. Ducha Świętego',
+    'ul. Sybiraków 2 15-204 Białystok',
+    ['06:30', '07:30', '09:00', '10:30', '12:00', '13:30', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Matki Bożej Fatimskiej',
+    'ul. Kraszewskiego 15b 15-024 Białystok',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. MB Różańcowej',
+    'ul. Narewska 26 15-840 Białystok',
+    ['08:00', '09:30', '11:00', '12:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Miłosierdzia Bożego',
+    'Pl. bł. ks. Michała Sopoćki 15-863 Białystok',
+    ['07:00', '09:00', '10:30', '11:45', '12:30', '15:00', '18:00', '20:00'],
+    ['06:30', '07:00', '15:00', '16:30', '18:00']
+  ],
+  [
+    'Parafia pw. Najświętszego Serca Jezusa',
+    'ul. Traugutta 25 15-145 Białystok',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Niepokalanego Serca Maryi',
+    'ul. Ks. Stanisława Suchowolca 27 15-565 Białystok',
+    ['08:00', '10:00', '11:30', '13:00', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. NMP Królowej Rodzin',
+    'ul. NMP Królowej Rodzin 6 15-684 Białystok',
+    ['07:00', '09:00', '10:30', '12:00', '14:00', '18:00', '19:30'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. NMP Matki Kościoła',
+    'ul. Pogodna 63 15-365 Białystok',
+    ['07:00', '08:30', '10:00', '11:30', '13:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. NMP Nieustającej Pomocy',
+    'ul. Czarnej Hańczy 56 15-161 Białystok',
+    ['09:00', '10:30', '12:00', '18:00'],
+    ['18:00']
+  ],
+  [
+    'Parafia pw. NMP z Guadalupe',
+    'ul. Wrocławska 8 Białystok 15-644',
+    ['07:00', '08:00', '10:00', '11:30', '13:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Przemienienia Pańskiego',
+    'ul. Klepacka 11 15-634 Białystok',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Andrzeja Boboli',
+    'ul. Św. Andrzeja Boboli 49a 15-649 Białystok',
+    ['07:00', '09:15', '11:00', '12:30', '16:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Anny',
+    'ul. Prezydenta R. Kaczorowskiego 5 Białystok 15-301',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Faustyny Kowalskiej',
+    'ul. Mikołajczyka 7 15-772 Białystok',
+    ['09:00', '10:30', '12:00', '13:30', '17:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Floriana',
+    'ul. H. Ordonówny 3 Białystok 15-750',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Jadwigi Królowej',
+    'ul. Słonecznikowa 8 15-664 Białystok',
+    ['06:30', '08:00', '09:30', '11:00', '12:30', '14:00', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Jana Chrzciciela',
+    'ul. Stoczni Gdańskiej 69 15-565 Białystok',
+    ['07:00', '08:10', '09:30', '11:00', '12:30', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Wojskowa pw. św. Jerzego',
+    'ul. Kawaleryjska 70 (koresp., osiedle domów szeregowych  przy ul. Rzymowskiego 43 (siedziba  15-325 Białystok',
+    ['07:30', '09:00', '10:30', '12:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Józefa Oblubieńca NMP',
+    'ul. św. Józefa 12 15-198 Białystok',
+    ['09:00', '10:30', '12:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Karola Boromeusza',
+    'ul. Pułaskiego 94 15-338 Białystok',
+    ['07:00', '08:30', '10:00', '12:00', '18:00', '19:30'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. św. Kazimierza Królewicza',
+    'ul. Św. Kazimierza 2 15-817 Białystok',
+    ['07:00', '08:00', '09:30', '11:00', '12:30', '16:30', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Krzysztofa',
+    'ul. św. Krzysztofa 4 15-697 Białystok',
+    ['08:00', '10:00', '18:00'],
+    ['08:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Maksymiliana Marii Kolbego',
+    'ul. Św. Maksymiliana 8 15-174 Białystok',
+    ['07:00', '09:00', '11:00', '12:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. O. Pio',
+    'ul. Mickiewicza 54a 15-232 Białystok',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Rafała Kalinowskiego',
+    'ul. Nowosielska 49 15-617 Białystok',
+    ['07:00', '09:00', '11:00', '12:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Rocha',
+    'ul. Ks. A. Abramowicza 1 15-872 Białystok',
+    ['06:30', '08:00', '09:30', '11:00', '12:30', '18:00'],
+    ['06:30', '08:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Stanisława B.M.',
+    'ul. Wiadukt 2B 15-327 Białystok',
+    ['08:00', '10:00', '11:30', '13:00', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. św. Wojciecha B.M.',
+    'ul. Warszawska 46a 15-077 Białystok',
+    ['06:30', '08:00', '09:30', '11:00', '12:30', '18:00', '19:15'],
+    ['06:30', '07:00', '07:30', '18:00']
+  ],
+  [
+    'Parafia pw. Świętej Rodziny',
+    'ul. Ogrodowa 2a 15-011 Białystok',
+    ['07:00', '08:30', '10:00', '11:30', '16:30', '18:00'],
+    ['07:00', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Wniebowzięcia NMP Katedra',
+    'ul. Kościelna 2 15-087 Białystok',
+    ['06:00', '08:00', '09:30', '11:00', '13:00', '16:30', '18:00', '19:30'],
+    ['06:00', '07:00', '08:00', '12:00', '18:00']
+  ],
+  [
+    'Parafia pw. Wszystkich Świętych',
+    'ul. Antoniuk Fabryczny 45 Białystok 15-762',
+    ['07:30', '09:30', '11:30', '13:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Zmartwychwstania Pańskiego',
+    'ul. Ks. Abp. R. Jałbrzykowskiego 5 Białystok 15-753',
+    ['07:30', '09:30', '11:00', '12:30', '18:00', '20:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Zwiastowania NMP',
+    'ul. A. Chętnika 80 15-166 Białystok',
+    ['07:00', '09:00', '11:00', '12:15', '17:00'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. NMP Królowej Polski',
+    'Boguszewo 96 Boguszewo 19-100 Mońki',
+    ['09:00', '11:00'],
+    ['07:30', '18:00']
+  ],
+  [
+    'Parafia pw. św. Jana Chrzciciela',
+    'Brzozowa 20 Brzozowa 19-124 Jaświły',
+    ['08:00', '10:00', '12:00'],
+    ['06:30']
+  ],
+  [
+    'Parafia pw. MB Pocieszenia',
+    'Chodorówka Nowa 34 Chodorówka Nowa 16-150 Suchowola',
+    ['09:00', '11:30'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. św. Jana Chrzciciela i św. Szczepana M.',
+    'ul. Mickiewicza 2 Choroszcz 16-070',
+    ['08:00', '09:30', '11:00', '12:15', '14:00', '15:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Jezusa Miłosiernego',
+    'ul. Żeromskiego 2 Czarna Białostocka 16-020',
+    ['07:00', '09:00', '10:30', '12:00', '17:00'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. Świętej Rodziny',
+    'ul. Czajkowskiego 2 Czarna Białostocka 16-020',
+    ['08:00', '10:00', '11:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. MB Anielskiej',
+    'ul. Ks. Szapela 9 Czarna Wieś Kościelna 16-020 Czarna Białostocka',
+    ['08:00', '10:00'],
+    ['17:00']
+  ],
+  [
+    'Parafia pw. św. Stanisława B.M.',
+    'ul. 3 Maja 28 Dąbrowa Białostocka 16-200',
+    ['07:00', '09:00', '11:00', '14:30', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Zwiastowania NMP',
+    'Dobrzyniewo Kościelne 39 Dobrzyniewo 16-002',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Wawrzyńca',
+    'Dolistowo 19-124 Jaświły',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. MB Anielskiej',
+    'Downary-Plac 22 Downary 19-110 Goniądz',
+    ['09:00', '11:30'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. św. Franciszka z Asyżu',
+    'ul. Białostocka 57 Fasty 15-694',
+    ['09:00', '11:00', '18:00'],
+    ['08:00', '08:00']
+  ],
+  [
+    'Parafia pw. Nawiedzenia NMP',
+    'Giełczyn 19-105 Laskowiec',
+    ['10:00'],
+    ['18:30']
+  ],
+  [
+    'Parafia pw. św. Agnieszki',
+    'ul. Mickiewicza 15 19-110 Goniądz',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. Krzyża Świętego',
+    'ul. Białostocka 76 Grabówka 15-523',
+    ['07:00', '09:00', '11:00', '12:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. MB Wspomożenia Wiernych',
+    'Grodzisk 1 Grodzisk 16-150 Suchowola',
+    ['08:30', '11:00', '13:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. Najświętszego Serca Pana Jezusa',
+    'ul. Białostocka 31 Gródek 16-040',
+    ['09:00', '10:45', '13:00', '14:00'],
+    ['18:00']
+  ],
+  [
+    'Parafia pw. Przemienienia Pańskiego',
+    'Hołodolina 19 Hołodolina 16-150 Suchowola',
+    ['09:00', '11:30'],
+    ['07:30']
+  ],
+  [
+    'Parafia pw. św. Jana Pawła II',
+    'ul. Hryniewicka 13 Ignatki-Osiedle 16-001',
+    ['07:00', '10:00', '12:00', '19:30'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. Przemienienia Pańskiego',
+    'ul. Kościelna 1 Jałówka 16-050 Michałowo',
+    ['09:00', '11:00'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. św. Jerzego',
+    'ul. Kościelna 3 Janów 16-130',
+    ['07:00', '09:00', '11:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Świętej Trójcy',
+    'ul. Kościelna 1 19-122 Jasionówka',
+    ['08:00', '09:30', '11:30', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Najświętszego Serca Pana Jezusa',
+    'Jaświły 100 19-124 Jaświły',
+    ['09:00', '12:00'],
+    ['07:30']
+  ],
+  [
+    'Parafia pw. MB Pocieszenia',
+    'Jatwieź Duża 16-150 Suchowola',
+    ['07:00', '09:00', '11:30'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. Świętej Trójcy',
+    'ul. Królowej Rodzin 2 Juchnowiec 16-061',
+    ['08:00', '09:30', '11:30'],
+    ['07:00', '17:00', '19:00']
+  ],
+  [
+    'Parafia pw. Chrystusa Dobrego Pasterza',
+    'ul. Jana Pawła II 1 Jurowce 16-010 Wasilków',
+    ['07:30', '09:00', '10:30', '12:00', '18:00'],
+    ['17:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Anny',
+    'Kalinówka Kościelna 39 Kalinówka 19-120 Knyszyn',
+    ['07:30', '09:30', '11:30', '13:30', '13:30'],
+    ['07:00', '17:00', '18:00']
+  ],
+  [
+    'Parafia pw. Wniebowstąpienia Pańskiego',
+    'Karakule, ul. Kościelna 1 Karakule 16-030 Supraśl',
+    ['09:00', '11:00'],
+    ['15:00', '16:00', '17:00']
+  ],
+  [
+    'Parafia pw. św. Teresy od Dzieciątka Jezus',
+    'ul. Zambrowska 24 Kleosin 16-001',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Świętej Trójcy i św. Dominika',
+    'Klimówka 28 Klimówka 16-123 Kuźnica',
+    ['08:00', '10:00'],
+    ['07:30']
+  ],
+  [
+    'Parafia pw. św. Jana Ap. i Ew.',
+    'ul. Kościelna 3 Knyszyn 19-120',
+    ['07:30', '09:00', '11:00', '13:00', '13:00', '18:00'],
+    ['06:30', '07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Najśw. Serca Pana Jezusa',
+    'Konowały 1 Konowały 16-070 Choroszcz',
+    ['09:00', '11:00'],
+    ['18:00']
+  ],
+  [
+    'Parafia pw. NMP Królowej Polski',
+    'Kopisk 24 Kopisk 16-002 Dobrzyniewo Kościelne',
+    ['09:00', '11:00'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. Znalezienia i Podwyższenia Krzyża Świętego',
+    'ul. Białostocka 23 Korycin 16-140',
+    ['08:00', '09:30', '11:30', '17:00', '18:00'],
+    ['06:30']
+  ],
+  [
+    'Parafia pw. NMP Nieustającej Pomocy',
+    'Kozińce 20B Kozińce 16-002 Dobrzyniewo Duże',
+    ['08:30', '10:30'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. MB Miłosierdzia',
+    'Krosno 6 Krosno 16-080 Tykocin',
+    ['09:00', '11:30'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. św. Anny',
+    'ul. Nowa 1 Krynki 16-120',
+    ['08:00', '10:00', '12:00', '17:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. Narodzenia NMP',
+    'Krypno Kościelne 29 Krypno 19-111 Krypno Kościelne',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Józefa Rzemieślnika',
+    'ul. Szkolna 5 Księżyno 16-005',
+    ['08:00', '10:00', '12:00', '17:00'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. św. Klary',
+    'Kulesze 28 Kulesze 19-100 Mońki',
+    ['09:00', '12:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. Narodzenia NMP',
+    'Kundzin 2 Kundzin 16-100 Sokółka',
+    ['08:00', '10:00'],
+    ['07:30']
+  ],
+  [
+    'Parafia pw. Opatrzności Bożej',
+    'Plac Tysiąclecia 15 Kuźnica 16-123',
+    ['07:00', '09:00', '10:30', '17:00'],
+    ['06:30', '07:00', '17:00']
+  ],
+  [
+    'Parafia pw. Najśw. Serca Pana Jezusa',
+    'Laskowiec 8 Laskowiec 19-105',
+    ['08:30', '12:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. MB Nieustającej Pomocy i św. Rocha',
+    'Lipina 89 Lipina - Janowszczyzna 16-100 Sokółka',
+    ['09:00', '11:00'],
+    ['07:00', '07:30', '16:30', '18:00']
+  ],
+  [
+    'Parafia pw. św. Józefa Rzemieślnika i św. Faustyny Kowalskiej',
+    'Łubianka 22 Łubianka 16-130 Janów',
+    ['09:00', '11:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. MB Częstochowskiej i św. Kazimierza',
+    'Majewo Kościelne 1 Majewo 16-140 Sokółka',
+    ['09:00', '11:00'],
+    ['07:30']
+  ],
+  [
+    'Parafia pw. Niepokalanego Serca Maryi',
+    'Bombla 32 Marianowo 16-140 Korycin',
+    ['08:00', '10:30'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. Opatrzności Bożej',
+    'ul. Żwirki i Wigury 49 Michałowo 16-050',
+    ['08:00', '09:30', '11:00', '18:00'],
+    ['07:00', '08:00', '18:00']
+  ],
+  [
+    'Parafia pw. NMP Pośredniczki Łask i św. Agaty',
+    'Minkowce 73 Minkowce 16-113 Szudziałowo',
+    ['09:00', '10:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. MB Częstochowskiej i św. Kazimierza',
+    'ul. Ks. Małynicza 1 19-100 Mońki',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Brata Alberta Chmielowskiego',
+    'Al. Wojska Polskiego 47 19-101 Mońki',
+    ['08:00', '10:00', '12:00', '17:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Jana Chrzciciela',
+    'ul. Hajnowska 15 17-220 Narewka',
+    ['09:00', '11:00'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. św. Antoniego Padewskiego',
+    'Niewodnica Kościelna, ul. Kościelna 3 Niewodnica 18-106 Turośń Kościelna',
+    ['07:00', '09:00', '10:30', '10:30', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Jana Chrzciciela',
+    'ul. Kościelna 12 Nowy Dwór 16-205',
+    ['08:00', '09:15', '11:00', '14:00'],
+    ['07:00', '07:30', '18:00']
+  ],
+  [
+    'Parafia pw. MB Miłosierdzia',
+    'Podlipki 1 Podlipki 16-120 Krynki',
+    ['10:00'],
+    []
+  ],
+  [
+    'Parafia pw. Miłosierdzia Bożego',
+    'Pogorzałki 111 Pogorzałki 16-002 Dobrzyniewo',
+    ['08:30', '11:00', '18:00'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. Najśw. Serca Jezusowego',
+    'Rozedranka Stara 8 Rozedranka Stara 16-100 Sokółka',
+    ['09:00', '11:00'],
+    ['18:00']
+  ],
+  [
+    'Parafia pw. Ofiarowania NMP',
+    'Różanystok Różanystok 16-200 Dąbrowa Białostocka',
+    ['07:30', '09:00', '10:30', '11:45', '16:00'],
+    ['06:30', '07:10', '17:00']
+  ],
+  [
+    'Parafia pw. NMP Częstochowskiej',
+    'Rynki 10 Rynki 18-105 Suraż',
+    ['09:00', '11:30'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. Opatrzności Bożej',
+    'Siderka 1 Siderka 16-124 Sidra',
+    ['09:00', '11:00'],
+    ['07:30', '17:00', '18:00']
+  ],
+  [
+    'Parafia pw. Świętej Trójcy',
+    'ul. Kościelna 3 16-124 Sidra',
+    ['07:00', '09:00', '11:00'],
+    ['07:00', '07:30']
+  ],
+  [
+    'Parafia pw. Przemienienia Pańskiego',
+    'Sokolany 25 Sokolany 16-100 Sokółka',
+    ['07:00', '09:00', '11:00'],
+    ['07:00', '07:30']
+  ],
+  [
+    'Parafia pw. Najświętszego Ciała i Krwi Chrystusa',
+    'ul. Jana Pawła II 5 Sokółka 16-100',
+    ['07:00', '09:00', '11:00', '15:00', '17:00'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. św. Antoniego Padewskiego',
+    'ul. Grodzieńska 47 16-100 Sokółka',
+    ['06:30', '08:00', '09:30', '11:00', '15:00', '18:00'],
+    ['06:30', '07:15', '15:00', '18:00']
+  ],
+  [
+    'Parafia pw. Wniebowzięcia NMP',
+    'ul. Targowa 5C Sokółka 16-100',
+    ['08:00', '10:00', '12:00', '19:30'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Apostołów Piotra i Pawła',
+    'Plac Kościuszki 3 16-150 Suchowola',
+    ['07:00', '09:00', '10:30', '12:00', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. NMP Królowej Polski',
+    'ul. J. Piłsudskiego 52B Supraśl 16-030',
+    ['07:30', '09:00', '11:00', '18:00'],
+    ['07:30', '18:00']
+  ],
+  [
+    'Parafia pw. Świętej Trójcy',
+    'ul. J. Piłsudskiego 9 Supraśl 16-030',
+    ['08:00', '10:00', '12:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Bożego Ciała',
+    'ul. Zakościelna 2 Suraż 18-105',
+    ['08:00', '10:00', '12:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. św. Wincentego Ferreriusza i św. Bartłomieja Apostoła',
+    'Kościelna 9A Szudziałowo 16-113',
+    ['08:00', '10:00', '12:00'],
+    ['08:00']
+  ],
+  [
+    'Parafia pw. Niepokalanego Poczęcia NMP',
+    'Tryczówka 1 Tryczówka 16-061 Juchnowiec',
+    ['06:00', '09:00', '10:00', '11:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. św. Apostołów Piotra i Pawła',
+    'ul. 3 Maja 33 19-104 Trzcianne',
+    ['07:00', '09:30', '11:00', '12:30'],
+    ['07:00', '17:00']
+  ],
+  [
+    'Parafia pw. Świętej Trójcy',
+    'ul. Białostocka 1 Turośń Kościelna 18-106',
+    ['09:00', '12:00', '17:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. św. Wojciecha B.M.',
+    'ul. Kościelna 8 Uhowo 18-100 Łapy',
+    ['09:00', '11:30', '18:00'],
+    ['07:00']
+  ],
+  [
+    'Parafia pw. NMP Matki Miłosierdzia',
+    'ul. Kościuszki 2 16-010 Wasilków',
+    ['07:00', '09:00', '11:00', '18:00'],
+    ['07:00', '18:00']
+  ],
+  [
+    'Parafia pw. Przemienienia Pańskiego',
+    'ul. Kościelna 57 16-010 Wasilków',
+    ['07:00', '08:30', '10:00', '11:30', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. św. Stanisława Kostki',
+    'ul. Białostocka 108 (kaplica tymczasowa Wasilków 16-010',
+    ['09:00', '11:00', '20:00'],
+    ['18:30']
+  ],
+  [
+    'Parafia pw. Matki Bożej Bolesnej',
+    'ul. Ks. Rabczyńskiego 2 Wasilków - Święta Woda 16-010',
+    ['08:00', '10:00', '12:00', '15:00', '18:00', '20:00'],
+    ['15:00', '18:00']
+  ],
+  [
+    'Parafia pw. św. Apostołów Piotra i Pawła',
+    'ul. Kościelna 2 16-060 Zabłudów',
+    ['07:00', '09:00', '10:00', '11:00', '12:30', '18:00'],
+    ['06:30', '18:00']
+  ],
+  [
+    'Parafia pw. NMP Pocieszenia',
+    'Zalesie 14 Zalesie 16-124 Sidra',
+    ['09:00', '11:00'],
+    ['07:30']
+  ],
+  [
+    'Parafia pw. NMP Królowej Polski',
+    'Zwierzyniec Wielki 13 Zwierzyniec Wielki 16-200 Dąbrowa Białostocka',
+    ['08:30', '11:30'],
+    ['07:30', '17:00', '19:00']
+  ],
+];
