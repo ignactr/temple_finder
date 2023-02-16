915c4d9f73f81e133376f3d85d1feaf8c50dc675
Ignacy Trocki <ignacy.trocki@wp.pl>
2023-02-16 12:58:51 +0100

    added route between destination and source

--- lib/patchfind.dart
+++ lib/patchfind.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:flutter_geocoder/geocoder.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class PatchFind extends StatefulWidget {
   final LatLng coordsOfDestination;
@@ -21,6 +21,28 @@ class _PatchFindState extends State<PatchFind> {
   _PatchFindState(this.coordsOfDestination, this.sourceLocation);
 
   final Completer<GoogleMapController> _controller = Completer();
+  List<LatLng> polylineCoordinates = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        "AIzaSyCquYlfmsgzS99xqkDw17cY8RWaH1og-EI",
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(
+            coordsOfDestination.latitude, coordsOfDestination.longitude));
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) =>
+          polylineCoordinates.add(LatLng(point.latitude, point.longitude)));
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +51,13 @@ class _PatchFindState extends State<PatchFind> {
       markers: {
         Marker(markerId: const MarkerId("początek"), position: sourceLocation),
         Marker(markerId: const MarkerId("cel"), position: coordsOfDestination)
+      },
+      polylines: {
+        Polyline(
+            polylineId: const PolylineId("route"),
+            points: polylineCoordinates,
+            color: const Color(0xFF334155),
+            width: 5),
       },
       onMapCreated: (mapController) {
         _controller.complete(mapController);
